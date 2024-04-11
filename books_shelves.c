3e446471f9a31eee11c3b97132a29771ef873bd4
Bill Rees <uberfoot@yahoo.com>
2024-04-11 16:51:23 -0700

    Malloc to little memory: size_t versus int

--- books_shelves.c
+++ books_shelves.c
@@ -24,13 +24,17 @@ int main()
     int total_number_of_queries;
     scanf("%d", &total_number_of_queries);
    
-    total_number_of_pages = (int**)malloc(sizeof(int) * total_number_of_shelves);
+    total_number_of_pages = (int**)malloc(total_number_of_shelves * sizeof(size_t));
+    memset(total_number_of_pages, 0, total_number_of_shelves * sizeof(size_t));
     for (int i = 0; i < total_number_of_shelves; i++)
     {
-            total_number_of_pages[i] = (int*) malloc(BOOKS_PER_SHELF * sizeof(int));
+        total_number_of_pages[i] = (int*) malloc(BOOKS_PER_SHELF * sizeof(size_t));
     }
-    total_number_of_books = (int *)malloc(sizeof(int) * total_number_of_shelves); 
-
+    for (int i = 0; i < total_number_of_shelves; i++)
+    {
+        memset(total_number_of_pages[i], 0, BOOKS_PER_SHELF );
+    }
+    total_number_of_books = (int *)malloc(sizeof(size_t) * total_number_of_shelves); 
     while (total_number_of_queries--) {
         int type_of_query;
         scanf("%d", &type_of_query);
@@ -41,7 +45,6 @@ int main()
              */
             int x, y;
             scanf("%d %d", &x, &y);
-            fprintf(stderr, "shelf %d with %d pages\n", x, y);
             total_number_of_pages[x][total_number_of_books[x]] = y;
             total_number_of_books[x] += 1;
         } else if (type_of_query == 2) {
@@ -58,13 +61,11 @@ int main()
     if (total_number_of_books) {
         free(total_number_of_books);
     }
-    fprintf(stderr, "==A\n");
     for (int i = 0; i < total_number_of_shelves; i++) {
         if (*(total_number_of_pages + i)) {
             free(total_number_of_pages[i]);
         }
     }
-    fprintf(stderr, "==B\n");
     
     if (total_number_of_pages) {
         free(total_number_of_pages);
